04cf741c58636e428b8d66acdff7a7f6a80f2ac8
seungsoo525 <shinseungsoo@gmail.com>
2020-08-10 10:32:53 +0900

    1121212

---
+++ app/src/main/java/com/example/edcan_summervacation/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.edcan_summervacation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> )
+    }
+}
