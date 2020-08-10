3aa600b8cbd384faa24bfcb70a5ad4ed46323a3e
seungsoo525 <shinseungsoo@gmail.com>
2020-08-10 12:35:01 +0900

    1121212

--- app/src/main/java/com/example/edcan_summervacation/RegisterActivity.java
+++ app/src/main/java/com/example/edcan_summervacation/RegisterActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.edcan_summervacation.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     private ActivityRegisterBinding binding;
 
@@ -21,11 +27,32 @@ public class RegisterActivity extends AppCompatActivity {
         binding.setPwcheck("");
 
         binding.btnRegiSignup.setOnClickListener(view -> {
-            register(binding.getName(),binding.getEmail(),binding.getPw(),binding.getPwcheck());
+            register(binding.getName(), binding.getEmail(), binding.getPw(), binding.getPwcheck());
         });
     }
 
-    private void register(String name, String Email, String pw, String pwcheck){
-        
+    private void register(String name, String email, String pw, String pwcheck) {
+        if (name.isEmpty() || email.isEmpty() || pw.isEmpty() || pwcheck.isEmpty()) {
+            Toast.makeText(this, "빈칸을 입력해주세요", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!pw.equals(pwcheck)) {
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseFirestore
+                .collection("users")
+                .document(email)
+                .set(new UserModel(name,email))
+                .addOnSuccessListener(runnable -> {
+                    firebaseAuth
+                            .createUserWithEmailAndPassword(email, pw)
+                            .addOnSuccessListener(runnable1 -> {
+                                Toast.makeText(this, "정상적으로 가입되었습니다", Toast.LENGTH_SHORT).show();
+                                finish();
+                            });
+                });
+
     }
 }
